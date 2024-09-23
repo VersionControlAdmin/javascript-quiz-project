ce7e9859927a782a01b356e93bcdf54be1484453
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-09-23 17:42:42 +0200

    Done

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,47 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
     // 4. shuffleQuestions()
-
+    shuffleQuestions() {
+        this.questions.sort(() => Math.random()-0.5);
+    }
     // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        (this.questions[this.currentQuestionIndex].answer===answer) && this.correctAnswers++;
+    }
 
     // 6. hasEnded()
+    hasEnded() {
+        return this.currentQuestionIndex === this.questions.length;
+    }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (!(difficulty >= 1 && difficulty <= 3)) return;
+        this.questions = this.questions.filter(question => question.difficulty === difficulty);
+
+    }
+
+    averageDifficulty() {
+        let averageDifficulty = 0;
+        this.questions.forEach((element) => averageDifficulty += element.difficulty)
+        return averageDifficulty/this.questions.length;
+    }
 }
